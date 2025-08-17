880811e3549aea6496f0e6df3b77e132909af6f7
Andrii Medvediev <a.medvedev@me.com>
2025-08-18 00:59:14 +0200

    Refactor LED blinking logic in main.rs
    - Change main function signature to accept Spawner as a parameter.
    - Introduce a separate asynchronous task for LED blinking with adjustable interval.
    - Update logging message for clarity during LED blinking.
    - Maintain LED state toggling functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 #![no_std]
 #![no_main]
 
+use defmt::{info, unwrap};
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) -> ! {
+async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    let mut led = Output::new(p.PA5, Level::Low, Speed::Low);
+    let led = Output::new(p.PA5, Level::Low, Speed::Low);
 
+    unwrap!(spawner.spawn(blinker(led, Duration::from_millis(300))));
+}
+
+#[embassy_executor::task]
+async fn blinker(mut led: Output<'static>, interval: Duration) {
     loop {
         led.toggle();
-        defmt::info!("Blink");
-        Timer::after(Duration::from_millis(500)).await;
+        info!("Blink fn");
+        Timer::after(interval).await;
     }
 }
